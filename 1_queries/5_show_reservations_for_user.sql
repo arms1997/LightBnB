71ca006865e0a548c1100d92fa7faba8677338db
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-02-03 17:50:04 -0500

    made updates

--- 1_queries/5_show_reservations_for_user.sql
+++ 1_queries/5_show_reservations_for_user.sql
@@ -1,19 +1,19 @@
--- SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
--- FROM reservations
--- JOIN properties ON properties.id = reservations.property_id
--- JOIN property_reviews ON property_reviews.property_id = properties.id
--- WHERE reservations.guest_id = 1 
--- AND reservations.end_date < now()::date
--- GROUP BY reservations.id, properties.id
--- ORDER BY reservations.end_date
--- LIMIT 10;
-
-SELECT reservations.*, avg(rating) as average_rating
+SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
 FROM reservations
-JOIN properties ON reservations.property_id = properties.id
-JOIN property_reviews ON properties.id = property_reviews.property_id 
-WHERE reservations.guest_id = 1
+JOIN properties ON properties.id = reservations.property_id
+JOIN property_reviews ON property_reviews.property_id = properties.id
+WHERE reservations.guest_id = 1 
 AND reservations.end_date < now()::date
-GROUP BY properties.id, reservations.id
-ORDER BY reservations.start_date
+GROUP BY reservations.id, properties.id
+ORDER BY reservations.end_date
 LIMIT 10;
+
+-- SELECT reservations.*, avg(rating) as average_rating
+-- FROM reservations
+-- JOIN properties ON reservations.property_id = properties.id
+-- JOIN property_reviews ON properties.id = property_reviews.property_id 
+-- WHERE reservations.guest_id = 1
+-- AND reservations.end_date < now()::date
+-- GROUP BY properties.id, reservations.id
+-- ORDER BY reservations.start_date
+-- LIMIT 10;
